2f992c6ae909e4bdb6681509a2e13dcb84564cc5
Paul Arelt <paularelt@gmail.com>
2021-07-11 14:49:51 -0400

    Add insertion by index

--- main.c
+++ main.c
@@ -19,6 +19,15 @@ typedef struct person
 
 
 /*
+###########################################
+##########                       ##########
+##########  TRAVERSAL FUNCTIONS  ##########
+##########                       ##########
+###########################################
+*/
+
+
+/*
 Traverse the existing nodes looking for the end of the list
 Return a pointer to the last element in the list (Item that points to NULL)
 */
@@ -32,6 +41,41 @@ Person* traverse_till_null(Person* list)
       break;
 
     n = n->next_person;
+  }
+  
+  return n;
+}
+
+/*
+Traverse the existing nodes looking for the end of the list
+Return a pointer to the last element in the list (Item that points to NULL)
+*/
+Person* traverse_till_index(Person* list, const int index)
+{
+  Person* n = list;
+
+  if(index == 0)
+  {
+    printf("TTI: Start of list.\n");
+    return n;
+  }
+
+  int idx = 0;
+  while(1)
+  {
+    if (n->next_person == NULL)
+    {
+      printf("Reached end of the list. Returning.\n");
+      break;
+    }
+
+    if (idx == index - 1)
+    {
+      break;
+    }
+
+    n = n->next_person;
+    ++idx;
   }
   
   return n;
@@ -122,6 +166,9 @@ void add_person_begin(Person** list, const char* name)
 {
   if(number == 0)
   {
+    if(*list == NULL)
+      (*list) = (Person*) malloc(sizeof(Person));
+
     strcpy((*list)->name, name);
     (*list)->idx = number;
     (*list)->next_person = NULL;
@@ -184,6 +231,68 @@ void add_person_end(Person** list, const char* name)
 
   // Point the previous list node to the new node
   last_person->next_person = new_person;
+
+  ++number;
+}
+
+
+/*
+Add a new node to a particular index.
+*/
+void add_person_index(Person** list, const char* name, const unsigned int index)
+{  
+  if(number == 0 && index != 0)
+  {
+    printf("Index does not line up with number of elements. Returning\n");
+    return;
+  }
+
+  if(index == 0)
+  {
+    add_person_begin(list, name);
+    return;
+  }
+  else if(index == number - 1)
+  {
+    add_person_end(list, name);
+    return;
+  }
+  
+  // Carve out space on the heap for the new person's node
+  Person* new_person = (Person*) malloc(sizeof(Person));
+  Person* last_person = traverse_till_index((*list), index);
+
+  printf("last_person:");
+  print_person(last_person);
+
+  strcpy(new_person->name, name);
+  
+  // Update index of the new person
+  new_person->idx = index;
+
+  // rewire the pointers
+  printf("Rewiring\n");
+  new_person->next_person = last_person->next_person == NULL ? NULL : last_person->next_person;
+  last_person->next_person = new_person;
+
+  // Handle incrementing
+  Person* p = traverse_till_null(*list);
+
+  if(p->next_person == NULL)
+    return;
+
+  p = p->next_person;
+  ++p->idx;
+
+  printf("Incrementing latter\n");
+  while(1)
+  {
+    if(p->next_person == NULL)
+      break;
+
+    ++p->idx;
+    p = p->next_person;
+  }
 
   ++number;
 }
@@ -312,20 +421,11 @@ int main()
   // The global holder/reference point for all list nodes
   Person* list = NULL;
   
-  add_person_end(&list, "Paul");
   add_person_begin(&list, "Peter");
-  add_person_end(&list, "John");
-  add_person_begin(&list, "James");
+  add_person_end(&list, "James");
+  add_person_index(&list, "Paul", 0);
   
   print_list(list);
-  
-  /*
-  remove_before(&list);
-  remove_before(&list);
-  add_person_begin(&list, "Peter");
-  
-  print_list(list);
-  */
   
   free_list(&list);
   

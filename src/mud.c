d34fd0362bc91ea210597b4c59feeb24c4e3782e
parelt <parelt@localhost.localdomain>
2021-07-20 17:54:21 -0400

    Add a text based menu for adding,printing,deleting etc

---
+++ src/mud.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include "ll.h"
+
+
+void print_options()
+{
+    puts("---------- OPTIONS ----------------");
+    puts("1. Add Person Front");
+    puts("2. Add Person End");
+    puts("3. Add Person at Index");
+    puts("4. Remove Person Front");
+    puts("5. Remove Person End");
+    puts("6. Remove Person at Index");
+    puts("7. Print List");
+    puts("8. Print Options");
+    puts("9. Quit ");
+    puts("-----------------------------------");
+}
+
+
+/*
+Entrypoint.
+*/
+int main()
+{
+  // The global holder/reference point for all list nodes
+  Person* list = NULL;
+  int done = 0;
+  int pass = 0;
+  int option;
+
+  print_options();
+
+  while(!done)
+  {
+      puts("Enter your choice:");
+      scanf("%d", &option);
+      switch(option)
+      {
+          case 1:
+          {
+              char v[120];
+              puts("Enter person name:");
+              scanf("%s", v);
+              add_person_begin(&list, v);
+              pass = 0;
+              break;
+          }
+          case 2:
+          {
+              char v[120];
+              puts("Enter person name:");
+              scanf("%s", v);
+              add_person_end(&list, v);
+              pass = 0;
+              break;
+          }
+          case 3:
+          {
+              char v[120];
+              int idx;
+              puts("Enter person name:");
+              scanf("%s", v);
+              puts("Enter index:");
+              scanf("%d", &idx);
+              add_person_index(&list, v, idx);
+              pass = 0;
+              break;
+          }
+          case 4:
+          {
+              remove_before(&list);
+              pass = 0;
+              break;
+          }
+          case 5:
+          {
+              remove_after(&list);
+              pass = 0;
+              break;
+          }
+          case 6:
+          {
+              int idx;
+              puts("Enter index:");
+              scanf("%d", &idx);
+              remove_person_index(&list, idx);
+              pass = 0;
+              break;
+          }
+          case 7:
+          {
+              print_list(list);
+              pass = 0;
+              break;
+          }
+          case 8:
+          {
+              print_options();
+              pass = 0;
+              break;
+          }
+          case 9:
+          {
+              done = 1;
+              pass = 0;
+              break;
+          }
+          default:
+          {
+              printf("Cannot interpret command '%d' - quitting\n", option);
+              done = 1;
+          }
+      }
+      ++pass;
+      if(pass > 2)
+        done = 1;
+  }
+  free_list(&list);
+  puts("Goodbye.");
+
+  return 0;
+}
